e588bd578e02bf32aa2fc32d3ee5c589a0a556b2
wangfei <fwang@cloudpick.me>
2020-05-12 16:55:19 +0800

    fix

--- detectron2/modeling/backbone/__init__.py
+++ detectron2/modeling/backbone/__init__.py
@@ -4,5 +4,5 @@ from .build import build_backbone, BACKBONE_REGISTRY  # noqa F401 isort:skip
 from .backbone import Backbone
 from .fpn import FPN
 from .resnet import ResNet, ResNetBlockBase, build_resnet_backbone, make_stage
-
+from .efficientnet import EfficientNet
 # TODO can expose more resnet blocks after careful consideration
